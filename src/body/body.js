f573933dab85475218fc2f2dc522bd04b36b9d24
Matthew Messier <messier_matthew@yahoo.com>
2018-07-09 18:08:06 -0400

    Updates

--- src/body/body.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import './body.css';
-export class Body extends Component {
-    render() {
-        return (
-            <section className="p20">Body: Nav selection: {this.props.navFocus}</section>
-        );
-    }
-}
